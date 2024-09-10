f389d3cca6e7ae734a9942d9761a64ab2aa1058f
dedvadik <77642225+dedvadik@users.noreply.github.com>
2024-09-10 09:17:19 +0300

    write fiirst lines

--- ProjectMain/Program.cs
+++ ProjectMain/Program.cs
@@ -1,1 +1,30 @@
-﻿Console.WriteLine("Hello, World!");
+﻿/*
+string value1 = " a";
+string value2 = "A ";
+Console.WriteLine(value1.Trim().ToLower() == value2.Trim().ToLower());
+
+System.Console.WriteLine("a" != "a");
+System.Console.WriteLine("a" != "A");
+System.Console.WriteLine(1 != 2);
+string myvalue = "a";
+System.Console.WriteLine(myvalue != "a");
+
+System.Console.WriteLine(1 > 2);
+System.Console.WriteLine(1 < 2);
+System.Console.WriteLine(1 >= 2);
+System.Console.WriteLine(1 <= 2);
+
+string text = "The quick brown fox jumps over the lazy dog.";
+System.Console.WriteLine(text.Contains("fox") == false);
+System.Console.WriteLine(!text.Contains("cow"));
+
+string text = "I`m wtite this text, but this no code!";
+System.Console.WriteLine(!text.Contains("this"));
+System.Console.WriteLine(!text.Contains("don`t"));
+*/
+int a = 7;
+int b = 6;
+Console.WriteLine(a != b); // output: True
+string s1 = "Hello";
+string s2 = "Hello";
+Console.WriteLine(s1 != s2); // output: False
